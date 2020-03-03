a6c95e7af9a7d7c17bd0ab1cd35029c454c0beeb
KhalilMay <khello1996@gmail.com>
2020-03-03 15:52:17 +0100

    Changes from original github

--- examples/batterystats/Scripts/interaction.py
+++ examples/batterystats/Scripts/interaction.py
@@ -10,13 +10,15 @@ def main(device, *args, **kwargs):
     print("Waiting for log...")
     results = {}
     logcat_output = device.logcat_regex("Master-Thesis:").splitlines()
+    print(logcat_output)
     for x in logcat_output:
-        if 'Load' in x:
+        if 'Load' in str(x,'utf-8'):
+            x= str(x,'utf-8')
             results = eval("{" + x.split("{")[1].split("}")[0] + "}")
             break
     print(logcat_output)
     print(results)
-    with open(kwargs['output_dir'] + '/batterystats/' + 'load_' + 'results_{}_{}.csv'.format(device.id, time.strftime('%Y.%m.%d_%H%M%S')), 'wb') as f:  # Just use 'w' mode in 3.x 'w' mode in 3.x
+    with open(kwargs['output_dir'] + '/batterystats/' + 'load_' + 'results_{}_{}.csv'.format(device.id, time.strftime('%Y.%m.%d_%H%M%S')), 'w') as f:  # Just use 'w' mode in 3.x 'w' mode in 3.x
         w = csv.DictWriter(f, results.keys())
         w.writeheader()
         w.writerow(results)
