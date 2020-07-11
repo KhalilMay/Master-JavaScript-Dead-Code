aca08da3af153887d4647d7fe0a2de01ce9cf0aa
KhalilMay <khello1996@gmail.com>
2020-07-11 17:16:27 +0200

    remove LacunaV2 tool

--- LacunaV2-master/analyzers/TAJS/README.md
+++
@@ -1,4 +0,0 @@
-This algorithm was copied from https://github.com/cs-au-dk/TAJS
-Some minor changes were made to extract the output slightly easier.
-(which can be found in Monitoring.java.diff)
-
--- LacunaV2-master/call_graph-edge.js
+++
@@ -1,8 +0,0 @@
-/** represents a caller-callee relationship (directional edge) */
-module.exports = class Edge {
-    constructor(caller, callee, analyzer) {
-        this.caller = caller;
-        this.callee = callee;
-        this.analyzer = analyzer;
-    }
-}
